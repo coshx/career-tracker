44eb8281d63885bc110c21ef17ce38b75cd9d1b2
Mike Hickman <mhickman84@gmail.com>
2011-08-04 04:08:51 -0400

    resume controllers and views

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,2 +1,30 @@
 class ResumesController < ApplicationController
+  def index
+    @resumes = current_user.resumes
+  end
+
+  def new
+    @resume = Resume.new
+  end
+
+  def create
+    @resume = Resume.new(params[:resume])
+    current_user.resumes << @resume
+    if @resume.save
+      flash[:notice] = "Created Resume #{@resume.title}."
+      redirect_to :action => 'index'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def show
+    @resumes = current_user.resumes
+    @resume = Resume.find(params[:id])
+
+  end
+
+  def edit
+
+  end
 end
